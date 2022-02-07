cdcd7dceb8b7460760e33059cef6e3539cacb50e
MrSquiggles13 <ayresguitar@gmail.com>
2022-02-07 10:51:18 -0500

    Initial Folder Structure

---
+++ app/src/main/java/com/example/movieapidata/model/MovieApiRepo.kt
@@ -0,0 +1,4 @@
+package com.example.movieapidata.model
+
+object MovieApiRepo {
+}
---
+++ app/src/main/java/com/example/movieapidata/view/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.movieapidata.view
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.movieapidata.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
